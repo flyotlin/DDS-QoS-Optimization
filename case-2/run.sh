8155ee7cbe82fcdc8bfd2d3c72b9dc6ca666c7b1
flyotlin <flyotlin@gmail.com>
2022-08-24 22:57:14 +0800

    Refine run and playbook in case 2

--- case-2/run.sh
+++ case-2/run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 configs=("OMG-Def" "OMG-Mod" "MS-1" "MS-10" "MS-100" "MS-1000")
+messages=30000
+logDirname="collected_logs"
 
-
-for rate in "1000" "2000"
-do
-    for config in ${configs[@]}
-    do
-        for trialNum in {1..10}
-        do
-            printf "Start trial $trialNum with config $config, rate $rate\n"
-            time ansible-playbook -v -i inventory.yaml playbook.yml -e "trialNum=$trialNum config=$config messages=20000 rate=$rate"
+for rate in "1000" "2000"; do
+    for i in {1..2}; do
+        for config in ${configs[@]}; do
+            for trialNum in {1..10}; do
+                printf "Start trial $trialNum with config $config, rate $rate\n"
+                time ansible-playbook -v -i inventory.yaml playbook.yml -e "trialNum=$trialNum config=$config messages=$messages rate=$rate logDirname=$logDirname-$messages-$rate-$i max_timeout=50"
+            done
         done
     done
 done
